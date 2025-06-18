150c386106b18fe9b0a0cf932e40b176f94fe85b
Sahilgill24 <gursahilgill24@gmail.com>
2025-06-18 21:21:07 +0530

    challenge: Broken Discrete log

---
+++ broken_dlog/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    printf("This is a C17 program.\n");
+    return 0;
+}
+ // CUDA trial implementation for the work 
+ // although 24gb is more than enough, the CPU ram is not 
+ // the PCIe bus is the bottleneck, not the GPU memory
